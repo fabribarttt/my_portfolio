7026f652e43cd60f668fd5b45a2acc3fc972255a
Fabricio Bartomeu <86902293+fabribarttt@users.noreply.github.com>
2025-04-12 20:14:35 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://fabribarttt.github.io",
-  base: "my_portfolio",
+  base: "/my_portfolio/",
 });
